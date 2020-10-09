dbf2169623fb5a19011448a8fc03179f0c260feb
Luiz Hammerli <>
2020-10-09 13:10:12 -0300

    Fisrt Commit

---
+++ TravelDiscovery/TravelDiscovery/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  TravelDiscovery
+//
+//  Created by Luiz Hammerli on 09/10/20.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
